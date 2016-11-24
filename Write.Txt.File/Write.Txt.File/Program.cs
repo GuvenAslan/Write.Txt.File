a42351af56f9333a219fbd34e1904a85d32d5681
guven <guvennaslan@gmail.com>
2016-11-25 00:16:11 +0200

    complated

---
+++ Write.Txt.File/Write.Txt.File/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write.Txt.File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "1. Line", "2. Line", "3. Line", "4. Line" };
+
+            System.IO.File.WriteAllLines(@"C:\testFolder\test.txt", lines);
+
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                "Morbi porttitor auctor sem ut lacinia." +
+                "Duis molestie egestas orci ut semper." +
+                "Sed nec magna in nulla pellentesque tristique." +
+                "Nulla luctus sem aliquet tincidunt sodales. Quisque tincidunt viverra ante eu euismod." +
+                "Donec sed massa lorem. Praesent in elit tristique, faucibus tellus eget, ornare neque." +
+                "Donec consequat, lorem eget dapibus malesuada, urna justo bibendum enim, vitae aliquam est urna vitae eros. Sed bibendum laoreet tortor eget pretium.";
+
+            System.IO.File.AppendAllText(@"C:\testFolder\test.txt", text);
+
+            using (System.IO.StreamWriter txtFile = new System.IO.StreamWriter(@"C:\testFolder\test.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    txtFile.WriteLine(line);
+                }
+                txtFile.WriteLine(text);
+            }
+            Console.ReadKey();
+        }
+    }
+}
